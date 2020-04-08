17b6a8c8ecfd233380f17b2e09d756d73f33dc7e
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-04-08 21:19:19 +0300

    add data and menu

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,17 @@ import Sidebar from './components/Sidebar'
 import Main from './components/Main'
 export default {
   name: 'App',
+  data () {
+    return {
+      peoples: [
+        { id: 1, fullName: 'Oleh Lev', position: 'Web Dev', skill: 'PHP,JS', exp: 3, sex: 'Male', salary: 4500 },
+        { id: 2, fullName: 'John White', position: 'Web Dev', skill: 'PHP', exp: 1, sex: 'Male', salary: 1200 },
+        { id: 3, fullName: 'Jany Rad', position: 'Sale', skill: '-', exp: 2, sex: 'Famale', salary: 3500 },
+        { id: 4, fullName: 'Ivan Brown', position: 'iOS', skill: 'Swift', exp: 3, sex: 'Male', salary: 4000 },
+        { id: 5, fullName: 'Pet Bool', position: 'Android', skill: 'Java', exp: 2, sex: 'Male', salary: 3520 },
+      ]
+    }
+  },
   components: {
     Header,
     Sidebar,
---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="menu">
+
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Menu',
+    components: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
